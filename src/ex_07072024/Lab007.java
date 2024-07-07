eb4843d883b2ba7cbe15367d9f2b10b03d2699a6
Abhimansinghyadav <abhimansinghyadav@gmail.com>
2024-07-07 19:23:16 +0530

    file is added

---
+++ src/ex_07072024/Lab007.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab007
+{
+    public static void main(String [] args)
+    {
+        System.out.println("lab008");
+        System.out.println("lab007");
+
+
+    }
+
+}
